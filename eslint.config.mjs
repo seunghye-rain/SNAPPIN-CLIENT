01f75957926f14b5d996e7bb302d04871cd54120
Tnalxmsk <alstnwkd990@naver.com>
2025-12-30 22:11:53 +0900

    chore: 스토리북 관련 의존성 및 ESLint 설정 정리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -13,6 +16,17 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+
+  // Storybook 스토리 파일 전용 린트 규칙
+  {
+    files: ['**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+    plugins: {
+      storybook,
+    },
+    rules: {
+      ...storybook.configs.recommended.rules,
+    },
+  },
 ]);
 
 export default eslintConfig;
