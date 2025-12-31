88401da3d3d5073523b83f1f6cc369a7d069392e
민하 <sminha8590@naver.com>
2025-12-31 18:35:47 +0900

    feat: 배럴파일 생성

---
+++ src/ui/chip/index.ts
@@ -0,0 +1,3 @@
+export { default as Chip } from './Chip';
+export { default as StateChip } from './state-chip/StateChip';
+export { default as TagChip } from './tag-chip/TagChip';
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,2 +1,3 @@
 // ⚠️ 자동 생성된 파일입니다. 직접 수정하지 마세요.
+export * from './chip/index';
 export * from './dialog/index';
