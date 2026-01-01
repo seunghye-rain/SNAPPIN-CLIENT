381cc3ae7a3e77e0f08b82751a7243562e57528c
bhyerain <seunghye0307@naver.com>
2026-01-01 22:38:05 +0900

    chore: export

---
+++ src/ui/section-tabs/index.ts
@@ -0,0 +1,10 @@
+export { default as SectionTabs } from './SectionTabs';
+
+export {
+  SECTION_TAB,
+  SECTION_TAB_LABEL,
+  SECTION_TAB_PRESET,
+  getSectionTabLabel,
+} from './types/sectionTabTheme';
+
+export type { SectionTabsProps } from './types/sectionTab.types';
