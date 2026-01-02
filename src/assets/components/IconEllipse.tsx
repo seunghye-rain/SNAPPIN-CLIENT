e51f9b09648b6743beb05516fd10656193556bd9
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-02 12:49:26 +0900

    feat: icon 추가

---
+++ src/assets/components/IconEllipse.tsx
@@ -0,0 +1,19 @@
+/**
+ * ⚠️ 자동 생성된 파일입니다. 직접 수정하지 마세요.
+ */
+
+import type { SVGProps } from 'react';
+
+const IconEllipse = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={7}
+    height={7}
+    fill="none"
+    {...props}
+    viewBox="0 0 7 7"
+  >
+    <circle cx={3.5} cy={3.5} r={3.5} fill="currentColor" />
+  </svg>
+);
+export default IconEllipse;
---
+++ src/assets/components/IconFilter.tsx
@@ -0,0 +1,43 @@
+/**
+ * ⚠️ 자동 생성된 파일입니다. 직접 수정하지 마세요.
+ */
+
+import type { SVGProps } from 'react';
+
+const IconFilter = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+    viewBox="0 0 24 24"
+  >
+    <mask
+      id="prefix__a"
+      width={24}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill="currentColor" d="M0 0h24v24H0z" />
+    </mask>
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M4 8h9m4 0h3m-9 8h9M4 16h3"
+    />
+    <path
+      stroke="#000"
+      strokeWidth={1.5}
+      d="M9 18a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM15 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+    />
+  </svg>
+);
+export default IconFilter;
