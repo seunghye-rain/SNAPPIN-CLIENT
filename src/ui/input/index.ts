0118838e22a2ca2112d1ab8612bfe76f23e0f4ce
Tnalxmsk <alstnwkd990@naver.com>
2026-01-02 22:02:52 +0900

    refactor: 코드레빗 리뷰 반영

--- src/ui/input/index.ts
+++ src/ui/input/index.ts
@@ -1,3 +1,3 @@
 export { default as Input } from './base/Input';
 export { default as TextField } from './field/TextField';
-export { default as FiledMessage } from './message/FieldMessage';
+export { default as FieldMessage } from './message/FieldMessage';
