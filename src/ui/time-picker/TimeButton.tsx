da76703825749036fb3fda73ef9249a0eb8ce168
bhyerain <seunghye0307@naver.com>
2026-01-03 02:34:50 +0900

    feat: TimeButton 스타일 및 로직 개선

--- src/ui/time-picker/TimeButton.tsx
+++ src/ui/time-picker/TimeButton.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/utils/cn';
-import { TimeButtonState } from './constants/buttonState';
+
+type TimeButtonState = 'default' | 'clicked' | 'disabled';
 
 type TimeButtonProps = {
   time: string;
@@ -7,30 +8,26 @@ type TimeButtonProps = {
   onClick?: (time: string) => void;
 };
 
-export function TimeButton({ time, state = 'default', onClick }: TimeButtonProps) {
+export const TimeButton = ({ time, state = 'default', onClick }: TimeButtonProps) => {
   const isDisabled = state === 'disabled';
 
   return (
     <button
       type='button'
       disabled={isDisabled}
-      onClick={() => {
-        if (state !== 'disabled') {
-          onClick?.(time);
-        }
-      }}
+      onClick={isDisabled ? undefined : () => onClick?.(time)}
       className={cn(
         'caption-14-md',
         'flex items-center justify-center w-[7.4rem] h-[3.3rem] transition-colors',
         'rounded border',
-        state === 'default' && 'border-black-4 text-black-10 bg-white',
+        state === 'default' && 'border-black-4 text-black-10 bg-black-1',
 
         state === 'clicked' && 'border-neon-white bg-neon-white text-black-10',
 
-        state === 'disabled' && 'text-black-6 cursor-not-allowed border-gray-200 bg-white',
+        state === 'disabled' && 'text-black-6 cursor-not-allowed border-black-4 bg-black-1',
       )}
     >
       {time}
     </button>
   );
-}
+};
