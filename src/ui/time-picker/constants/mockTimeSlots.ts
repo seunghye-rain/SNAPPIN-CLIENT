6a5a681ce6931f513a8a606313ca42fd5dff515e
bhyerain <seunghye0307@naver.com>
2026-01-03 02:34:11 +0900

    feat: mockTimeSlots 업데이트

--- src/ui/time-picker/constants/mockTimeSlots.ts
+++ src/ui/time-picker/constants/mockTimeSlots.ts
@@ -1,10 +1,16 @@
-import { TimeSlotSection } from '../types/timeSlot';
+import type { TimeSlotSection } from '../TimePicker';
 
 export const MOCK_TIME_SLOTS: TimeSlotSection[] = [
   {
     label: '오전',
     slots: [
-      { time: '09:00', disabled: true },
+      { time: '06:00', disabled: true },
+      { time: '06:30', disabled: true },
+      { time: '07:00', disabled: false },
+      { time: '07:30', disabled: false },
+      { time: '08:00', disabled: true },
+      { time: '08:30', disabled: false },
+      { time: '09:00', disabled: false },
       { time: '09:30', disabled: false },
       { time: '10:00', disabled: false },
       { time: '10:30', disabled: false },
@@ -18,9 +24,15 @@ export const MOCK_TIME_SLOTS: TimeSlotSection[] = [
       { time: '12:00', disabled: false },
       { time: '12:30', disabled: false },
       { time: '13:00', disabled: false },
-      { time: '13:30', disabled: false },
+      { time: '13:30', disabled: true },
       { time: '14:00', disabled: false },
-      { time: '14:30', disabled: true },
+      { time: '14:30', disabled: false },
+      { time: '15:00', disabled: false },
+      { time: '15:30', disabled: false },
+      { time: '16:00', disabled: false },
+      { time: '16:30', disabled: false },
+      { time: '17:00', disabled: true },
+      { time: '17:30', disabled: false },
     ],
   },
 ];
