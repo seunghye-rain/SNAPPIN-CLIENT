dba9265fbbf12b7db456a0b4b41bada41c30a90f
민하 <sminha8590@naver.com>
2026-01-03 13:46:51 +0900

    feat: 스토리에 설명, 다양한 예시 추가

--- src/ui/product-card/ProductCard.stories.tsx
+++ src/ui/product-card/ProductCard.stories.tsx
@@ -7,6 +7,11 @@ const meta: Meta<typeof ProductCard> = {
   component: ProductCard,
   parameters: {
     layout: 'centered',
+    docs: {
+      description: {
+        component: '프로덕트 카드 컴포넌트입니다.',
+      }
+    }
   },
   tags: ['autodocs'],
   argTypes: {
@@ -58,7 +63,7 @@ const meta: Meta<typeof ProductCard> = {
       src: '/product.png',
       alt: '임시 이미지',
     },
-    name: '찰나의 순간을 기억으로 남기는 소중한 촬영',
+    name: '잊지 못 할 졸업스냅',
     rating: 4.8,
     reviewCount: 20,
     author: '김작가',
@@ -71,3 +76,33 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const LongProductName: Story = {
+  args: {
+    image: {
+      src: '/product.png',
+      alt: '임시 이미지',
+    },
+    name: '찰나의 순간을 기억으로 남기는 소중한 촬영',
+    rating: 4.8,
+    reviewCount: 20,
+    author: '김작가',
+    price: 80000,
+    tags: ['WARM', 'ANALOG', 'DIRECTED'],
+  }
+}
+
+export const LongAuthorName: Story = {
+  args: {
+    image: {
+      src: '/product.png',
+      alt: '임시 이미지',
+    },
+    name: '잊지 못 할 졸업스냅',
+    rating: 4.8,
+    reviewCount: 20,
+    author: '작가 이름이 매우 긴 경우에는 이렇게 나와요',
+    price: 80000,
+    tags: ['WARM', 'ANALOG', 'DIRECTED'],
+  }
+}
