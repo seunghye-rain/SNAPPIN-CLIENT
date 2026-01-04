a7b4919299da031af7d6fc50b1cca7575b7173e3
Tnalxmsk <alstnwkd990@naver.com>
2026-01-04 18:51:22 +0900

    feat: Divider 굵기, 색상 정의

--- src/ui/divider/constants/theme.ts
+++ src/ui/divider/constants/theme.ts
@@ -1,4 +1,9 @@
-import { DividerColor } from '@/ui/divider/type/variant';
+import { DividerColor, DividerSize } from '@/ui/divider/type/variant';
+
+export const DIVIDER_THICKNESS: Record<NonNullable<DividerSize>, string> = {
+  small: 'h-[0.1rem]',
+  large: 'h-[0.6rem]',
+};
 
 export const DIVIDER_COLOR: Record<NonNullable<DividerColor>, string> = {
   'gray-dark': 'bg-black-5',
--- src/ui/divider/type/variant.ts
+++ src/ui/divider/type/variant.ts
@@ -1,1 +1,2 @@
+export type DividerSize = 'small' | 'large';
 export type DividerColor = 'gray-dark' | 'gray-light';
