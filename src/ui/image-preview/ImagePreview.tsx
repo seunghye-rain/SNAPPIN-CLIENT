9f9c723e076beb52ff5d326ee7fbfe80392529d9
bhyerain <seunghye0307@naver.com>
2026-01-05 14:20:12 +0900

    feat: 리뷰 반영

--- src/ui/image-preview/ImagePreview.tsx
+++ src/ui/image-preview/ImagePreview.tsx
@@ -4,10 +4,12 @@ import { cn } from '@/utils/cn';
 import { IconClose } from '@/assets';
 
 type ImagePreviewProps = {
-  imageSrc?: string;
-  imageAlt?: string;
+  imageSrc: string;
+  imageAlt: string;
   handleRemove?: () => void;
+  handleClickImage?: () => void;
   className?: string;
+  imageClassName?: string;
   showRemoveButton?: boolean;
   imageWidthRem?: number;
   imageHeightRem?: number;
@@ -17,32 +19,42 @@ export default function ImagePreview({
   imageSrc,
   imageAlt,
   handleRemove,
+  handleClickImage,
   className,
+  imageClassName,
   showRemoveButton = true,
   imageWidthRem = 14,
   imageHeightRem = 14,
 }: ImagePreviewProps) {
+  const handleRemoveClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    handleRemove?.();
+  };
   const imagePreviewStyle = {
     width: `${imageWidthRem}rem`,
     height: `${imageHeightRem}rem`,
   };
 
   return (
-    <div className={cn('relative overflow-hidden', className)} style={imagePreviewStyle}>
+    <div
+      className={cn('relative overflow-hidden', handleClickImage ? 'cursor-pointer' : null, className)}
+      style={imagePreviewStyle}
+      onClick={handleClickImage}
+    >
       {imageSrc ? (
         <Image
           src={imageSrc}
-          alt={imageAlt ?? '이미지 미리보기'}
+          alt={imageAlt}
           fill
           sizes={`${imageWidthRem}rem`}
-          className='object-cover'
+          className={cn('object-cover', imageClassName)}
         />
       ) : (
         <div className='bg-black-3 h-full w-full' />
       )}
       {imageSrc && showRemoveButton && handleRemove ? (
         <IconButton
-          onClick={handleRemove}
+          onClick={handleRemoveClick}
           className='bg-black-1 absolute top-[0.7rem] right-[0.7rem] flex h-[3.2rem] w-[3.2rem] items-center justify-center rounded-full'
         >
           <IconClose className='text-black-10 h-[2.4rem] w-[2.4rem]' />
