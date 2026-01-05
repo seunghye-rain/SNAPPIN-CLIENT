fa002c168f9a557ac2dd6edc8614e45ee9f3c5d3
민하 <sminha8590@naver.com>
2026-01-06 03:42:20 +0900

    refactor: FilterChipProps 변경
    - status 삭제
    - isSelected, onClick, onRemove 추가

--- src/ui/chip/filter-chip/FilterChip.tsx
+++ src/ui/chip/filter-chip/FilterChip.tsx
@@ -1,27 +1,36 @@
 import { cn } from '@/utils/cn';
 import { IconButton } from '@/ui/button';
 import { IconClose } from '@/assets';
-import { FilterChipStatus } from './types/filterChipStatus';
+import { FilterChipVariant } from './types/filterChipVariant';
 import { FILTER_CHIP_THEME } from './constants/filterChipTheme';
 // TODO: 공용 타입으로 변경
 import { TagCode } from '../tag-chip/types/tagCode';
 import { TAG_LABEL } from '../tag-chip/constants/tagLabel';
 
+const getVariant = (isSelected: boolean, onRemove?: (tag: TagCode) => void): FilterChipVariant => {
+  if (onRemove) { return 'selectedRemovable'; }
+  else if (isSelected) { return 'selected'; }
+  else { return 'default'; }
+}
+
 export type FilterChipProps = {
   label: TagCode;
-  status: FilterChipStatus;
-  onClick: () => void;
-}
+  isSelected: boolean;
+  onClick?: (tag: TagCode) => void;
+  onRemove?: (tag: TagCode) => void;
+} & Omit<React.HTMLAttributes<HTMLDivElement>, 'onClick'>;
 
 export default function FilterChip({
   label,
-  status,
+  isSelected,
   onClick,
+  onRemove,
   ...props
 }: FilterChipProps) {
-  const { buttonClassName, labelClassName } = FILTER_CHIP_THEME[status];
-  const isSelected = status === 'selected' || status === 'removable';
-  const removable = status === 'removable';
+  if (!isSelected && onRemove) { throw new Error('불가능한 조합입니다.'); }
+
+  const variant = getVariant(isSelected, onRemove);
+  const { buttonClassName, labelClassName } = FILTER_CHIP_THEME[variant];
 
   return (
     <div
@@ -34,14 +43,14 @@ export default function FilterChip({
       <button
         aria-label={`${TAG_LABEL[label]} 필터`}
         aria-pressed={isSelected}
-        onClick={!removable ? onClick : undefined}
+        onClick={onClick ? () => onClick(label) : undefined}
       >
         <span className={cn('caption-12-md transition-[color] duration-500 ease-in-out', labelClassName)}>
           {TAG_LABEL[label]}
         </span>
       </button>
-      { removable && (
-        <IconButton onClick={onClick}>
+      { onRemove && (
+        <IconButton onClick={() => onRemove(label)}>
           <IconClose
             className='w-[1.5rem] h-[1.5rem] m-[0.4rem] text-black-7'
           />
