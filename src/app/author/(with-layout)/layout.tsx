6f7321b791af07025d02a64d4d543e3d2a4ecf3a
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-05 17:28:24 +0900

    style : 스크롤 바 x

--- src/app/author/(with-layout)/layout.tsx
+++ src/app/author/(with-layout)/layout.tsx
@@ -4,7 +4,7 @@ import { Footer } from '@/ui';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='relative flex h-screen flex-col'>
-      <div className='mb-footer flex-1 overflow-y-auto'>{children}</div>
+      <div className='scrollbar-hide flex-1 overflow-y-auto'>{children}</div>
       <Footer userRole='author' />
     </div>
   );
