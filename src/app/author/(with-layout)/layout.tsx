73d44065ad0c513fa6b5629430f450277935b1ee
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-07 14:46:14 +0900

    style: author layout css 수정

--- src/app/author/(with-layout)/layout.tsx
+++ src/app/author/(with-layout)/layout.tsx
@@ -3,8 +3,8 @@ import { Footer } from '@/ui';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex h-screen flex-col'>
-      <div className='scrollbar-hide flex-1 overflow-y-auto'>{children}</div>
+    <div className='flex flex-col'>
+      {children}
       <Footer userRole='author' />
     </div>
   );
