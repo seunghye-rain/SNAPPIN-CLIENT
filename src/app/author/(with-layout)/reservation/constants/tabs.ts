47283a3418f659625f4519d1c2a360b49c50a6ef
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-07 19:57:39 +0900

    feat: 메뉴 tab menu

---
+++ src/app/author/(with-layout)/reservation/constants/tabs.ts
@@ -0,0 +1,18 @@
+export const RESERVATION_TABS = [
+  {
+    label: '예약 요청',
+    value: 'PHOTOGRAPHER_REQUESTED',
+  },
+  {
+    label: '조율 중',
+    value: 'PHOTOGRAPHER_ADJUSTING',
+  },
+  {
+    label: '예약 확정',
+    value: 'PHOTOGRAPHER_CONFIRMED',
+  },
+  {
+    label: '촬영 완료',
+    value: 'PHOTOGRAPHER_DONE',
+  },
+];
