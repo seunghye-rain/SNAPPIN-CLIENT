a7401400ac67d239b061d8dd36bbe52e6efc95c6
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-07 20:17:54 +0900

    refactor: 서버 enum 반영

--- src/types/stateCode.ts
+++ src/types/stateCode.ts
@@ -1,9 +1,9 @@
 export type StateCode =
   | 'RESERVATION_REQUESTED' // 예약 요청
-  | 'AUTHOR_CONFIRMING' // 작가 확인 중
+  | 'PHOTOGRAPHER_CHECKING' // 작가 확인 중
   | 'PAYMENT_REQUESTED' // 결제 요청
   | 'PAYMENT_COMPLETED' // 결제 완료
-  | 'RESERVATION_COMPLETED' // 예약 확정
+  | 'RESERVATION_CONFIRMED' // 예약 확정
   | 'RESERVATION_REFUSED' // 예약 거절
-  | 'RESERVATION_CANCELLED' // 예약 취소
+  | 'RESERVATION_CANCELED' // 예약 취소
   | 'SHOOT_COMPLETED'; // 촬영 완료
--- src/ui/chip/state-chip/constants/stateChipTheme.ts
+++ src/ui/chip/state-chip/constants/stateChipTheme.ts
@@ -3,11 +3,11 @@ import { StateCode } from '@/types/stateCode';
 
 export const STATE_CHIP_THEME_BY_LABEL: Record<StateCode, ChipTheme> = {
   RESERVATION_REQUESTED: { chipClassName: 'bg-yellow', labelClassName: 'text-yellow-text' },
-  AUTHOR_CONFIRMING: { chipClassName: 'bg-yellow', labelClassName: 'text-yellow-text' },
+  PHOTOGRAPHER_CHECKING: { chipClassName: 'bg-yellow', labelClassName: 'text-yellow-text' },
   PAYMENT_REQUESTED: { chipClassName: 'bg-yellow', labelClassName: 'text-yellow-text' },
   PAYMENT_COMPLETED: { chipClassName: 'bg-blue', labelClassName: 'text-blue-text' },
-  RESERVATION_COMPLETED: { chipClassName: 'bg-green', labelClassName: 'text-green-text' },
+  RESERVATION_CONFIRMED: { chipClassName: 'bg-green', labelClassName: 'text-green-text' },
   RESERVATION_REFUSED: { chipClassName: 'bg-red', labelClassName: 'text-red-text' },
-  RESERVATION_CANCELLED: { chipClassName: 'bg-red', labelClassName: 'text-red-text' },
+  RESERVATION_CANCELED: { chipClassName: 'bg-red', labelClassName: 'text-red-text' },
   SHOOT_COMPLETED: { chipClassName: 'bg-black-4', labelClassName: 'text-black-8' },
 };
