34286352a59be5e0fc10d3dbf1b73eb3f647238b
bhyerain <seunghye0307@naver.com>
2026-01-11 05:01:11 +0900

    feat: 배럴 파일

---
+++ src/app/client/(with-layout)/reservation/components/index.ts
@@ -0,0 +1,2 @@
+export { default as HeaderNavigation } from './header-navigation/HeaderNavigation';
+export { default as ReservationContent } from './reservation-content/ReservationContent';
---
+++ src/app/client/reservation-detail/[id]/components/index.ts
@@ -0,0 +1,1 @@
+export { default as HeaderNavigation } from './header-navigation/HeaderNavigation';
