87400df3b2de2014e44fd960f0e8248aefee9313
민하 <sminha8590@naver.com>
2026-01-10 19:55:31 +0900

    feat: 배럴 파일 생성

---
+++ src/app/(with-layout)/portfolio-detail/[id]/components/index.ts
@@ -0,0 +1,4 @@
+export { default as DetailInfoList } from './DetailInfoList';
+export { default as Header } from './Header';
+export { default as LikeButton } from './LikeButton';
+export { default as PortfolioCarousel } from './PortfolioCarousel';
