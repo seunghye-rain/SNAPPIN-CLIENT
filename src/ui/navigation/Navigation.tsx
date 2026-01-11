bcd3c656de64a295bef35cef461efa33aae8f700
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-11 10:55:24 +0900

    style: nav bg color

--- src/ui/navigation/Navigation.tsx
+++ src/ui/navigation/Navigation.tsx
@@ -17,11 +17,13 @@ export default function Navigation({
   isSticky = false,
   isFixed = false,
 }: NavigationProps) {
+  const bgClass = className?.split(' ').find((cls) => cls.startsWith('bg')) ?? 'bg-black-1';
+
   return (
     <>
       <nav
         className={cn(
-          'bg-black-1 grid h-[5rem] w-full grid-cols-3 items-center px-[2rem] py-[1.7rem]',
+          'bg-black-1 nav-height grid w-full grid-cols-3 items-center px-[2rem] py-[1.7rem]',
           isSticky && 'sticky top-0 z-10',
           isFixed && 'fixed-center top-0 z-10',
           className,
@@ -31,7 +33,7 @@ export default function Navigation({
         <div className='justify-self-center'>{center}</div>
         <div className='justify-self-end'>{right}</div>
       </nav>
-      {isFixed && <div className='bg-black-1 h-[5rem]' />}
+      {isFixed && <div className={cn(bgClass + ' nav-height')} />}
     </>
   );
 }
