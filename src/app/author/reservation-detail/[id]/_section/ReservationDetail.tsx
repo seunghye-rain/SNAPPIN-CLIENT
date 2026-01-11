b3dfdfed6b62f18b29b56a8324be6a329e733842
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-12 08:52:19 +0900

    feat: 예약 durationTime format util 구현

--- src/app/author/reservation-detail/[id]/_section/ReservationDetail.tsx
+++ src/app/author/reservation-detail/[id]/_section/ReservationDetail.tsx
@@ -2,6 +2,7 @@ import { StateCode } from '@/types/stateCode';
 import { DetailLayout, DetailRow } from '../components/detail-layout/DetailLayout';
 import { formatCreatedAt, formatReservationDateTime } from '@/utils/formatNumberWithComma';
 import { Divider } from '@/ui';
+import { formatReservationTime } from '../utils/reservationTime';
 
 type ReservationDetailProps = {
   status: StateCode;
@@ -39,7 +40,7 @@ export default function ReservationDetail({
         <Divider thickness='small' color='bg-black-5' />
       </div>
       <DetailRow label='날짜 및 시간' value={formatReservationDateTime(date, startTime)} />
-      <DetailRow label='촬영 시간' value={`${durationTime / 60}시간`} />
+      <DetailRow label='촬영 시간' value={formatReservationTime(durationTime)} />
       <DetailRow label='촬영 장소' value={place} />
       <DetailRow label='촬영 인원' value={`${peopleCount}인`} />
       <DetailRow label='기타 요청 사항' value={requestNote} />
