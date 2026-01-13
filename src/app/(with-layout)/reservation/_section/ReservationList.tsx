3d64678c52540a6318ba284adc2645baca49b2a7
bhyerain <seunghye0307@naver.com>
2026-01-13 21:02:07 +0900

    refactor: 섹션탭

--- src/app/(with-layout)/reservation/_section/ReservationList.tsx
+++ src/app/(with-layout)/reservation/_section/ReservationList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect } from 'react';
 import { Divider, SectionTabs } from '@/ui';
 import { RESERVATION_MOCK } from '../mock/reservationList.mock';
 import { RESERVATION_TABS, type ReservationTabValue } from '../constants/tabs';
@@ -12,9 +12,6 @@ import { formatCreatedAt } from '@/utils/formatNumberWithComma';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 type ReservationListProps = {
-  params?: {
-    id: string;
-  };
   isNavigationVisible: boolean;
 };
 
@@ -24,10 +21,7 @@ export default function ReservationList({ isNavigationVisible }: ReservationList
   const tabQueryValue = searchParams.get('tab');
 
   const defaultTabValue = RESERVATION_TABS[0].value;
-
-  const [selectedTabValue, setSelectedTabValue] = useState<ReservationTabValue>(
-    (tabQueryValue ?? defaultTabValue) as ReservationTabValue,
-  );
+  const selectedTabValue = (tabQueryValue ?? defaultTabValue) as ReservationTabValue;
 
   useEffect(() => {
     if (tabQueryValue) {
@@ -38,13 +32,18 @@ export default function ReservationList({ isNavigationVisible }: ReservationList
   }, [defaultTabValue, router, tabQueryValue]);
 
   const handleTabValueChange = (value: string) => {
-    const nextTabValue = value as ReservationTabValue;
-
-    setSelectedTabValue(nextTabValue);
-    router.replace(`?tab=${nextTabValue}`, { scroll: false });
+    router.replace(`?tab=${value}`, { scroll: false });
   };
 
+  //TODO: 서버 데이터 연동, 파라미터에 selectedTabValue 추가
   const { reservations } = RESERVATION_MOCK;
+  const reservationsByTabValue = reservations.filter(({ reservation }) =>
+    selectedTabValue === 'CLIENT_DONE'
+      ? reservation.status === STATE_CODES.SHOOT_COMPLETED
+      : reservation.status !== STATE_CODES.SHOOT_COMPLETED,
+  );
+  const emptyTitle =
+    selectedTabValue === 'CLIENT_DONE' ? '촬영 완료한 상품이 없어요' : '예약 문의한 상품이 없어요';
 
   return (
     <SectionTabs
@@ -65,59 +64,44 @@ export default function ReservationList({ isNavigationVisible }: ReservationList
         ))}
       </SectionTabs.List>
 
-      {RESERVATION_TABS.map((tab) => {
-        const reservationsByTabValue = reservations.filter(({ reservation }) =>
-          tab.value === 'CLIENT_DONE'
-            ? reservation.status === STATE_CODES.SHOOT_COMPLETED
-            : reservation.status !== STATE_CODES.SHOOT_COMPLETED,
-        );
-        const emptyTitle =
-          tab.value === 'CLIENT_DONE' ? '촬영 완료한 상품이 없어요' : '예약 문의한 상품이 없어요';
-
-        return (
-          <SectionTabs.Contents key={tab.value} value={tab.value}>
-            {reservationsByTabValue.length === 0 ? (
-              <EmptyView
-                title={emptyTitle}
-                description='탐색에서 다양한 포트폴리오를 확인해보세요'
-              />
-            ) : (
-              <div className='mt-[4.5rem] flex flex-col p-[2rem]'>
-                {reservationsByTabValue.map(({ reservation }, reservationIndex) => (
-                  <Fragment key={reservation.reservationId}>
-                    <ReservationCard
-                      image={{
-                        src: reservation.product.imageUrl,
-                        alt: reservation.product.title,
-                      }}
-                      date={formatCreatedAt(reservation.createdAt)}
-                      name={reservation.product.title}
-                      rating={reservation.product.rate}
-                      reviewCount={reservation.product.reviewCount}
-                      author={reservation.product.photographer}
-                      price={reservation.product.price}
-                      tags={reservation.product.moods as MoodCode[]}
-                      isReviewed={reservation.product.isReviewed}
-                      reviewHref={`/photo-completed/${reservation.reservationId}`}
-                      status={reservation.status as StateCode}
-                      href={
-                        tab.value === 'CLIENT_DONE'
-                          ? `/photo-completed/${reservation.reservationId}`
-                          : `/reservation-detail/${reservation.reservationId}`
-                      }
-                    />
-                    {reservationIndex !== reservationsByTabValue.length - 1 ? (
-                      <div className='-mx-[2rem] py-[1.2rem]'>
-                        <Divider className='h-[0.6rem]' color='bg-black-3' />
-                      </div>
-                    ) : null}
-                  </Fragment>
-                ))}
-              </div>
-            )}
-          </SectionTabs.Contents>
-        );
-      })}
+      <SectionTabs.Contents value={selectedTabValue}>
+        {reservationsByTabValue.length === 0 ? (
+          <EmptyView title={emptyTitle} description='탐색에서 다양한 포트폴리오를 확인해보세요' />
+        ) : (
+          <div className='mt-[4.5rem] flex flex-col p-[2rem]'>
+            {reservationsByTabValue.map(({ reservation }, reservationIndex) => (
+              <Fragment key={reservation.reservationId}>
+                <ReservationCard
+                  image={{
+                    src: reservation.product.imageUrl,
+                    alt: reservation.product.title,
+                  }}
+                  date={formatCreatedAt(reservation.createdAt)}
+                  name={reservation.product.title}
+                  rating={reservation.product.rate}
+                  reviewCount={reservation.product.reviewCount}
+                  author={reservation.product.photographer}
+                  price={reservation.product.price}
+                  tags={reservation.product.moods as MoodCode[]}
+                  isReviewed={reservation.product.isReviewed}
+                  reviewHref={`/photo-completed/${reservation.reservationId}`}
+                  status={reservation.status as StateCode}
+                  href={
+                    selectedTabValue === 'CLIENT_DONE'
+                      ? `/photo-completed/${reservation.reservationId}`
+                      : `/reservation-detail/${reservation.reservationId}`
+                  }
+                />
+                {reservationIndex !== reservationsByTabValue.length - 1 ? (
+                  <div className='-mx-[2rem] py-[1.2rem]'>
+                    <Divider className='h-[0.6rem]' color='bg-black-3' />
+                  </div>
+                ) : null}
+              </Fragment>
+            ))}
+          </div>
+        )}
+      </SectionTabs.Contents>
     </SectionTabs>
   );
 }
