0d1f670887c593e80432a58d95714bbb9c2c2cb1
Tnalxmsk <alstnwkd990@naver.com>
2026-01-15 02:30:55 +0900

    feat: 빌드 에러 해결

--- src/app/(with-layout)/like/page.tsx
+++ src/app/(with-layout)/like/page.tsx
@@ -1,5 +1,10 @@
 import PageClient from '@/app/(with-layout)/like/page.client';
+import { Suspense } from 'react';
 
 export default function Like() {
-  return <PageClient />;
+  return (
+    <Suspense fallback={null}>
+      <PageClient />
+    </Suspense>
+  );
 }
