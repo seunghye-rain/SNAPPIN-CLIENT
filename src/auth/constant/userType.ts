d698a5d06858315c19e86d05ace0083b6a0548cf
bhyerain <seunghye0307@naver.com>
2026-01-14 18:04:56 +0900

    feat: 상수 타입 업데이트

--- src/auth/constant/userType.ts
+++ src/auth/constant/userType.ts
@@ -1,7 +1,13 @@
-export const USER_TYPES = ['CLIENT', 'PHOTOGRAPHER'] as const;
-export const USER_TYPE_LABEL: Record<UserType, string> = {
-  CLIENT: '고객',
-  PHOTOGRAPHER: '작가',
-};
+export const USER_TYPE = {
+  CLIENT: 'CLIENT',
+  PHOTOGRAPHER: 'PHOTOGRAPHER',
+} as const;
 
-export type UserType = (typeof USER_TYPES)[number];
+export type UserType = (typeof USER_TYPE)[keyof typeof USER_TYPE];
+
+export const USER_TYPES = Object.values(USER_TYPE) as UserType[];
+
+export const USER_TYPE_LABEL: Record<UserType, string> = {
+  [USER_TYPE.CLIENT]: '고객',
+  [USER_TYPE.PHOTOGRAPHER]: '작가',
+};
