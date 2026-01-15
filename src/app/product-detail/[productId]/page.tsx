464401f983de55a8a3f0e66fe942a29446102548
민하 <sminha8590@naver.com>
2026-01-15 21:33:31 +0900

    feat: 상품 상세 페이지 구현

---
+++ src/app/product-detail/[productId]/page.tsx
@@ -0,0 +1,6 @@
+import ClientPage from './page.client';
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <ClientPage productId={id} />;
+}
