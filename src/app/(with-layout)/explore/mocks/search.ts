a84f08d91e52fda0ed6e55b84f443d90451b5af1
Tnalxmsk <alstnwkd990@naver.com>
2026-01-18 12:06:02 +0900

    feat: 검색 스냅 카테고리 목 데이터 추가

---
+++ src/app/(with-layout)/explore/mocks/search.ts
@@ -0,0 +1,26 @@
+export const MOCK_SNAP_CATEGORIES = [
+  {
+    key: 'GRADUATION',
+    label: '졸업스냅',
+  },
+  {
+    key: 'WEDDING',
+    label: '결혼스냅',
+  },
+  {
+    key: 'COUPLE',
+    label: '커플스냅',
+  },
+  {
+    key: 'DAILY',
+    label: '일상스냅',
+  },
+  {
+    key: 'FAMILY',
+    label: '가족스냅',
+  },
+  {
+    key: 'RECITAL',
+    label: '연주스냅',
+  },
+];
