db116c86c338aa975a5fd505086d28fc2b1a8f2b
민하 <sminha8590@naver.com>
2026-01-20 03:50:33 +0900

    feat: 작가 관련 쿼리키 생성

--- src/query-key/user.ts
+++ src/query-key/user.ts
@@ -17,4 +17,10 @@ export const USER_QUERY_KEY = {
     WISH: ['wish'],
     WISHED_PORTFOLIOS: () => [...USER_QUERY_KEY.WISH, 'portfolios'],
     WISHED_PRODUCTS: () => [...USER_QUERY_KEY.WISH, 'products'],
-  } as const;
+
+    // 작가
+    PHOTOGRAPHER: ['photographer'],
+    PHOTOGRAPHER_DETAIL: (id: number) => [...USER_QUERY_KEY.PHOTOGRAPHER, id],
+    PHOTOGRAPHER_PRODUCTS: (id: number) => [...USER_QUERY_KEY.PHOTOGRAPHER, id, 'products'],
+    PHOTOGRAPHER_PORTFOLIOS: (id: number) => [...USER_QUERY_KEY.PHOTOGRAPHER, id, 'portfolios'],
+} as const;
