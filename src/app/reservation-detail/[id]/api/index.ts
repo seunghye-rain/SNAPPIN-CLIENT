d15be49c3a7f7c94623d45256abdd28fb2f7ee1d
bhyerain <seunghye0307@naver.com>
2026-01-20 21:00:52 +0900

    feat: 결제 API 연동

--- src/app/reservation-detail/[id]/api/index.ts
+++ src/app/reservation-detail/[id]/api/index.ts
@@ -6,8 +6,11 @@ import {
   GetReservationDetailData,
   CancelReservationResponse,
   UpdateReservationCancelData,
+  UpdateReservationRequestPaymentData,
+  RequestPaymentReservationResponse,
 } from '@/swagger-api/data-contracts';
 
+// 예약 상세 조회 API
 export const useGetReservationDetail = (reservationId: number, isEnabled = true) => {
   const enabled = isEnabled && reservationId != null;
 
@@ -25,6 +28,7 @@ export const useGetReservationDetail = (reservationId: number, isEnabled = true)
   });
 };
 
+// 예약 취소 API
 export const useCancelReservation = () => {
   const queryClient = useQueryClient();
 
@@ -37,6 +41,31 @@ export const useCancelReservation = () => {
 
       if (!response.data) {
         throw new Error(`No data from /api/v1/reservations/${reservationId}/cancel`);
+      }
+
+      return response.data;
+    },
+    //  뮤테이션이 성공하면 React Query 캐시를 무효화해서 최신 데이터를 다시 받아오게 함
+    onSuccess: (_, reservationId) => {
+      queryClient.invalidateQueries({ queryKey: USER_QUERY_KEY.RESERVATION_DETAIL(reservationId) });
+      queryClient.invalidateQueries({ queryKey: USER_QUERY_KEY.RESERVATION_LISTS() });
+    },
+  });
+};
+
+// 결제 요청 API
+export const useRequestPayment = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<RequestPaymentReservationResponse, Error, number>({
+    mutationFn: async (reservationId) => {
+      const response = await apiRequest<UpdateReservationRequestPaymentData>({
+        endPoint: `/api/v1/reservations/${reservationId}/pay`,
+        method: 'PATCH',
+      });
+
+      if (!response.data) {
+        throw new Error(`No data from /api/v1/reservations/${reservationId}/pay`);
       }
 
       return response.data;
--- src/app/reservation-detail/[id]/page.client.tsx
+++ src/app/reservation-detail/[id]/page.client.tsx
@@ -11,7 +11,7 @@ import CancelModal from './@modal/(.)cancel-modal/CancelModal';
 import { useToast } from '@/ui/toast/hooks/useToast';
 import { ACCESS_TOKEN_COOKIE_NAME } from '@/auth/constant/cookie';
 import { useAuth } from '@/auth/hooks/useAuth';
-import { useGetReservationDetail, useCancelReservation } from './api';
+import { useGetReservationDetail, useCancelReservation, useRequestPayment } from './api';
 
 type ReservationDetailPageClientProps = {
   reservationId: string;
@@ -28,6 +28,7 @@ export default function PageClient({ reservationId }: ReservationDetailPageClien
   );
 
   const { mutate: cancelReservation } = useCancelReservation();
+  const { mutate: requestPayment } = useRequestPayment();
 
   const [cancelOpen, setCancelOpen] = useState(false);
 
@@ -75,8 +76,14 @@ export default function PageClient({ reservationId }: ReservationDetailPageClien
   };
 
   const handlePaymentConfirmClick = () => {
-    setReservationStatus(STATE_CODES.PAYMENT_COMPLETED);
-    // TODO: API 호출 로직 추가
+    requestPayment(Number(reservationId), {
+      onSuccess: (paymentResponse) => {
+        setReservationStatus(paymentResponse.status as StateCode);
+      },
+      onError: () => {
+        toast.error('결제 요청 중 오류가 발생했습니다. 다시 시도해주세요.');
+      },
+    });
   };
 
   const handleInquiryClick = () => {
