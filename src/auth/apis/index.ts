5434ee8f39c4795ed148903932f8ce9f924f3a34
민하 <sminha8590@naver.com>
2026-01-21 20:00:55 +0900

    feat: 유저 정보 조회 API 연동

--- src/auth/apis/index.ts
+++ src/auth/apis/index.ts
@@ -1,8 +1,14 @@
-//TODO: logout, refresh api 호출
+import { useQuery } from '@tanstack/react-query';
+import { apiRequest } from '@/api/apiRequest';
 import { SERVER_API_BASE_URL } from '@/api/constants/api';
+import { AUTH_QUERY_KEY } from '@/query-key/auth';
+import {
+  GetUserInfoResponse,
+  GetUserInfoData,
+} from '@/swagger-api/data-contracts';
+import { useAuth } from '../hooks/useAuth';
 
 export const getRefreshToken = async () => {
-  //TODO: 서버측 api 연결되면 refresh api 호출
   const refreshResponse = await fetch(`${SERVER_API_BASE_URL}/api/v1/auth/reissue`, {
     method: 'POST',
     credentials: 'include',
@@ -11,8 +17,23 @@ export const getRefreshToken = async () => {
   return refreshResponse;
 };
 
-export const logoutApi = async () => {
-  //TODO: logout api 호출
-  //deleteAccessToken
-  //deleteUserType
-};
+// 유저 정보 조회 API
+export const useGetUserInfo = () => {
+  const { isLogIn } = useAuth();
+
+  return useQuery<GetUserInfoResponse>({
+    queryKey: AUTH_QUERY_KEY.AUTH,
+    queryFn: async () => {
+      const res = await apiRequest<GetUserInfoData>({
+        endPoint: '/api/v1/users/me',
+        method: 'GET',
+      });
+
+      if (!res.data) {
+        throw new Error('/api/v1/users/me 응답에 데이터가 존재하지 않습니다.');
+      }
+      return res.data;
+    },
+    enabled: !!isLogIn,
+  });
+}
