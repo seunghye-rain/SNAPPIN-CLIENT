6ec8a8328e373f56ef285cf4a9b7a015e28651b9
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-22 23:15:49 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { USER_TYPE } from '@/auth/constant/userType';
+//import { USER_TYPE } from '@/auth/constant/userType';
 
 const LOGIN_RESTRICTED_PATHS = ['/login'];
 // 로그인이 필요한 페이지 (인증이 필요한 페이지)
