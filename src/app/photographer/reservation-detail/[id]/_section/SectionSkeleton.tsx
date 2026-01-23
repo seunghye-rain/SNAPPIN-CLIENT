3465a4b4f3a74cbbf1f3e08838496088e4741532
bhyerain <seunghye0307@naver.com>
2026-01-23 18:20:05 +0900

    refactor: 작가 예약 상세 스켈레톤

--- src/app/photographer/reservation-detail/[id]/_section/SectionSkeleton.tsx
+++ src/app/photographer/reservation-detail/[id]/_section/SectionSkeleton.tsx
@@ -4,26 +4,33 @@ import { DetailLayout } from '../components/detail-layout/DetailLayout';
 export default function SectionSkeleton() {
   return (
     <div>
-      <DetailLayout title='예약 상세'>
-        <div className='flex flex-col gap-[1.5rem]'>
-          <ProductCardSkeleton className='border-none py-0 px-0' />
-          <div className='bg-black-3 h-[2.7rem] w-full' />
+      <div className='bg-black-1 flex flex-col pt-[1.7rem] pb-[1.2rem]'>
+        <span className='caption-14-bd px-[2rem]'>예약 요청 상품</span>
+        <ProductCardSkeleton className='pt-[1.2rem]' />
+        <div className='px-[2rem]'>
+          <div className='bg-black-3 h-[2.7rem] w-full rounded-[0.6rem]' />
         </div>
+      </div>
+      <Divider thickness='large' color='bg-black-3' />
+
+      <DetailLayout title='예약 상세'>
+        <div className='mt-[0.2rem] -mb-[0.5rem] flex flex-col gap-[0.6rem]'>
+          <div className='bg-black-3 h-[2.0rem] w-[5.8rem]' />
+          <div className='bg-black-3 h-[1.3rem] w-[10.2rem]' />
+        </div>
+        <Divider className='-mb-[0.5rem]' />
+        <div className='bg-black-3 mb-[0.4rem] h-[1.2rem] w-[15rem]' />
+        {Array.from({ length: 5 }).map((_, index) => (
+          <div key={index} className='bg-black-3 h-[1.4rem] w-[15rem]' />
+        ))}
       </DetailLayout>
       <Divider thickness='large' color='bg-black-3' />
-      <DetailLayout title='예약 상세'>
-        <div className='flex flex-col gap-[1.5rem]'>
-          <div className='flex flex-col gap-[0.4rem]'>
-            <div className='flex items-center gap-[0.2rem]'>
-              <div className='bg-black-3 h-[2.4rem] w-[6rem]' />
-            </div>
-            <div className='bg-black-3 h-[1.6rem] w-[9rem]' />
-          </div>
-          <Divider thickness='small' color='bg-black-5' />
-        </div>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <div key={index} className='bg-black-3 h-[1.6rem] w-[15rem]' />
+      <DetailLayout title='결제 상세' className='gap-[1.5rem]'>
+        {Array.from({ length: 2 }).map((_, index) => (
+          <div key={index} className='bg-black-3 w-full] h-[1.4rem]' />
         ))}
+        <Divider thickness='small' color='bg-black-3' />
+        <div className='bg-black-3 h-[2.8rem] w-full' />
       </DetailLayout>
     </div>
   );
