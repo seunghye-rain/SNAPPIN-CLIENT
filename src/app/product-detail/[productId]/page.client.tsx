db96cda91775522f30c5b6c7ca61a70e0d127d58
민하 <sminha8590@naver.com>
2026-01-23 13:52:59 +0900

    refactor: 상품 상세 스켈레톤 안 뜨는 문제 해결

--- src/app/product-detail/[productId]/page.client.tsx
+++ src/app/product-detail/[productId]/page.client.tsx
@@ -21,10 +21,10 @@ export default function ClientPage({ productId }: { productId: string }) {
   const tabParam = searchParams.get('tab');
   const [selectedTab, setSelectedTab] = useState(tabParam ?? PRODUCT_TAB.PRODUCT_DETAIL);
 
-  const { data, isFetching } = useGetProductDetail(Number(productId));
+  const { data, isPending } = useGetProductDetail(Number(productId));
 
   const productInfo = {
-    snapCategory: data?.productInfo?.snapCategory ?? '-',
+    snapCategory: data?.productInfo?.snapCategory ?? 'asdfasdf',
     regions: data?.productInfo?.regions ?? [],
     moods: data?.productInfo?.moods ?? [],
     maxPeople: data?.productInfo?.maxPeople ?? '-',
@@ -60,7 +60,7 @@ export default function ClientPage({ productId }: { productId: string }) {
   return (
     <div>
       <Header />
-      {isFetching ? (
+      {isPending ? (
         <ProductDetailSkeleton selectedTab={selectedTab} />
       ) : (
         <>
