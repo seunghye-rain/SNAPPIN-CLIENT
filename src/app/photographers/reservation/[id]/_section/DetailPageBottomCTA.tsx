69cd14f63cc765f361d1cae8d6896334f0685cd7
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-02-18 13:55:19 +0900

    refactor: getButtonConfig refactor

--- src/app/photographers/reservation/[id]/_section/DetailPageBottomCTA.tsx
+++ src/app/photographers/reservation/[id]/_section/DetailPageBottomCTA.tsx
@@ -34,52 +34,40 @@ export default function DetailPageBottomCTA({
   today.setHours(0, 0, 0, 0);
   const isAfterStart = today >= reservationStart;
 
-  const labelAndDisabledConfig = (label: string, disabled: boolean): ButtonConfig => ({
+  const getCTAConfig = (label: string, disabled: boolean, onClick?: () => void): ButtonConfig => ({
     label,
     disabled,
+    onClick,
   });
 
   const getButtonConfig = (): ButtonConfig => {
-    if (status === STATE_CODES.PHOTOGRAPHER_CHECKING) {
-      return {
-        ...labelAndDisabledConfig('결제 요청하기', false),
-        onClick: () => {
-          router.push(PHOTOGRAPHERS_ROUTES.PAYMENT(reservationId));
-        },
-      };
-    }
-
-    if (status === STATE_CODES.PAYMENT_COMPLETED) {
-      return {
-        ...labelAndDisabledConfig('예약 확정하기', false),
-        onClick: () => {
-          confirmReservation(reservationId);
-        },
-      };
-    }
-
-    if (status === STATE_CODES.RESERVATION_CONFIRMED && isAfterStart) {
-      return {
-        ...labelAndDisabledConfig('촬영 완료하고 리뷰 요청하기', false),
-        onClick: () => {
-          completeReservation(reservationId);
-        },
-      };
-    }
-
     switch (status) {
+      case STATE_CODES.PHOTOGRAPHER_CHECKING:
+        return getCTAConfig('결제 요청하기', false, () => {
+          router.push(PHOTOGRAPHERS_ROUTES.PAYMENT(reservationId));
+        });
+      case STATE_CODES.PAYMENT_COMPLETED:
+        return getCTAConfig('예약 확정하기', false, () => {
+          confirmReservation(reservationId);
+        });
+      case STATE_CODES.RESERVATION_CONFIRMED: {
+        if (isAfterStart) {
+          return getCTAConfig('촬영 완료하고 리뷰 요청하기', false, () => {
+            completeReservation(reservationId);
+          });
+        }
+        return getCTAConfig('예약 확정', true);
+      }
       case STATE_CODES.PAYMENT_REQUESTED:
-        return labelAndDisabledConfig('결제 요청 중', true);
-      case STATE_CODES.RESERVATION_CONFIRMED:
-        return labelAndDisabledConfig('예약 확정', true);
+        return getCTAConfig('결제 요청 중', true);
       case STATE_CODES.SHOOT_COMPLETED:
-        return labelAndDisabledConfig('리뷰 요청 완료', true);
+        return getCTAConfig('리뷰 요청 완료', true);
       case STATE_CODES.RESERVATION_CANCELED:
-        return labelAndDisabledConfig('고객님의 예약 취소', true);
+        return getCTAConfig('고객님의 예약 취소', true);
       case STATE_CODES.RESERVATION_REFUSED:
-        return labelAndDisabledConfig('예약 거절 완료', true);
+        return getCTAConfig('예약 거절 완료', true);
       default:
-        return labelAndDisabledConfig('', true);
+        return getCTAConfig('', true);
     }
   };
 
