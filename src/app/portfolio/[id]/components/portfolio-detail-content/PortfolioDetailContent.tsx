8685275b9f4380618afd1ec9d709069e4e690a47
sminha <sminha8590@naver.com>
2026-02-20 15:02:55 +0900

    refactor: 리뷰 반영 - pending 상태의 쿼리도 클라이언트로 전달하도록 QueryClient 옵션 추가 - pending 상태일 때는 스켈레톤을 전송하도록 Suspense 추가 - PortfolioDetailSkeleton이 두 곳에서 재사용되므로 컴포넌트 분리

--- src/app/portfolio/[id]/components/portfolio-detail-content/PortfolioDetailContent.tsx
+++ src/app/portfolio/[id]/components/portfolio-detail-content/PortfolioDetailContent.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { useRef } from 'react';
-import { Divider, ProductCardSkeleton } from '@/ui';
+import { Divider } from '@/ui';
 import { MoodCode } from '@/types/moodCode';
+import PortfolioDetailSkeleton from './PortfolioDetailSkeleton';
 import { PhotographerSection, PortfolioSection, ProductSection } from '../../_section/index';
 import { useGetPortfolioDetail } from '../../api';
 import { useScrollRestoreOnParent } from '@/hooks/useScrollRestoreOnParent';
@@ -63,53 +64,3 @@ export default function PortfolioDetailContent({ id, isLogIn }: { id: string, is
     </>
   );
 }
-
-const PortfolioDetailSkeleton = () => {
-  return (
-    <section>
-      <div className='bg-black-3 aspect-[3/4] w-full' />
-      <div className='px-[2rem] py-[1.6rem]'>
-        <div className='flex h-[3rem] justify-between'>
-          <div className='bg-black-3 h-[2.5rem] w-[14.4rem] rounded-[0.2rem]' />
-          <div className='bg-black-3 h-[2.5rem] w-[4.6rem] rounded-[0.2rem]' />
-        </div>
-      </div>
-      <div className='flex flex-col gap-[0.8rem] p-[2rem]'>
-        <span className='caption-14-md text-black-10'>관련 정보</span>
-        <div className='border-black-5 flex flex-col gap-[1.2rem] rounded-[0.6rem] border-1 p-[1.6rem]'>
-          {Array.from({ length: 4 }).map((_, idx) => (
-            <div key={idx} className='flex h-[1.4rem] gap-[1rem]'>
-              <div className='bg-black-3 flex w-[8rem] rounded-[0.2rem]' />
-              {idx !== 3 ? (
-                <div className='bg-black-3 flex flex-1 rounded-[0.2rem]' />
-              ) : (
-                <div className='bg-black-3 flex w-[8.6rem] rounded-[0.2rem]' />
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
-      <Divider thickness='large' color='bg-black-3' />
-      <div className='flex gap-[1.2rem] p-[2rem]'>
-        <div className='bg-black-3 h-[6.4rem] w-[6.4rem] rounded-full' />
-        <div className='flex h-[7rem] flex-1 flex-col gap-[0.9rem]'>
-          <div className='flex flex-col gap-[0.4rem]'>
-            <div className='bg-black-3 h-[1.7rem] w-[3.7rem] rounded-[0.2rem]' />
-            <div className='bg-black-3 h-[1.2rem] w-[9rem] rounded-[0.2rem]' />
-          </div>
-          <div className='flex flex-col gap-[0.4rem]'>
-            <div className='bg-black-3 h-[1.2rem] w-[15.8rem] rounded-[0.2rem]' />
-            <div className='bg-black-3 h-[1.2rem] w-[3.7rem] rounded-[0.2rem]' />
-          </div>
-        </div>
-      </div>
-      <Divider thickness='large' color='bg-black-3' />
-      <div className='flex flex-col gap-[1.6rem] px-[2rem] py-[1.6rem]'>
-        <span className='font-16-bd'>이 작가님의 상품 살펴보기</span>
-        <div className='border-black-5 rounded-[0.6rem] border-1'>
-          <ProductCardSkeleton />
-        </div>
-      </div>
-    </section>
-  );
-};
--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -1,8 +1,15 @@
+import { Suspense } from 'react';
 import { cookies } from 'next/headers';
-import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+  defaultShouldDehydrateQuery
+} from '@tanstack/react-query';
 import { prefetchPortfolioDetail } from './api';
 import DetailHeader from '@/components/layout/detail/DetailHeader';
 import PortfolioDetailContent from './components/portfolio-detail-content/PortfolioDetailContent';
+import PortfolioDetailSkeleton from './components/portfolio-detail-content/PortfolioDetailSkeleton';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -10,17 +17,27 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
   const cookieStore = await cookies();
   const isLogIn = cookieStore.has('AccessToken');
   
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      dehydrate: {
+        shouldDehydrateQuery: (query) => 
+          query.state.status === 'pending' ||
+          defaultShouldDehydrateQuery(query),
+      },
+    },
+  });
   if (!Number.isNaN(Number(id))) {
-    await prefetchPortfolioDetail(queryClient, Number(id), isLogIn);
+    prefetchPortfolioDetail(queryClient, Number(id), isLogIn);
   }
 
   return (
     <main>
       <DetailHeader>포트폴리오 상세</DetailHeader>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <PortfolioDetailContent id={id} isLogIn={isLogIn} />
-      </HydrationBoundary>
+      <Suspense fallback={<PortfolioDetailSkeleton />}>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <PortfolioDetailContent id={id} isLogIn={isLogIn} />
+        </HydrationBoundary>
+      </Suspense>
     </main>
   );
 }
