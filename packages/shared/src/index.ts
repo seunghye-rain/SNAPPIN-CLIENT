28b66b08759329042876d4256021fc565611dedc
Tnalxmsk <alstnwkd990@naver.com>
2026-03-11 15:24:42 +0900

    refactor: shared 패키지 공용 타입 export 구조 정리

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,6 @@
 export * from './types/moodCode';
 export * from './types/stateCode';
+export * from './types/userType';
 export * from './lib/formatNumber';
 export * from './lib/formatPrice';
 export * from './constants/imageAccept';
