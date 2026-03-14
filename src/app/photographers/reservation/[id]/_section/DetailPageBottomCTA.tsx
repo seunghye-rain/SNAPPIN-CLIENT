103f3208b1d7cb19f64f3ad24a469881faa41b0c
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-03-14 14:01:12 +0900

    feat: ts-pattern 적용

--- src/app/photographers/reservation/[id]/_section/DetailPageBottomCTA.tsx
+++ src/app/photographers/reservation/[id]/_section/DetailPageBottomCTA.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { match } from 'ts-pattern';
 import { BottomCTAButton } from '@/ui';
 import { STATE_CODES, StateCode } from '@/types/stateCode';
 import { useCompleteReservation, useConfirmReservation } from '../api';
@@ -11,18 +12,6 @@ type DetailPageBottomCTAProps = {
   date: string;
   status: StateCode;
 };
-
-type ButtonConfig = {
-  label: string;
-  disabled: boolean;
-  onClick?: () => void;
-};
-
-const getCTAConfig = (label: string, disabled: boolean, onClick?: () => void): ButtonConfig => ({
-  label,
-  disabled,
-  onClick,
-});
 
 export default function DetailPageBottomCTA({
   reservationId,
@@ -40,40 +29,59 @@ export default function DetailPageBottomCTA({
   today.setHours(0, 0, 0, 0);
   const isAfterStart = today >= reservationStart;
 
-  const getButtonConfig = (): ButtonConfig => {
-    switch (status) {
-      case STATE_CODES.PHOTOGRAPHER_CHECKING:
-        return getCTAConfig('결제 요청하기', false, () => {
-          router.push(PHOTOGRAPHERS_ROUTES.PAYMENT(reservationId));
-        });
-      case STATE_CODES.PAYMENT_COMPLETED:
-        return getCTAConfig('예약 확정하기', false, () => {
-          confirmReservation(reservationId);
-        });
-      case STATE_CODES.RESERVATION_CONFIRMED: {
-        if (isAfterStart) {
-          return getCTAConfig('촬영 완료하고 리뷰 요청하기', false, () => {
-            completeReservation(reservationId);
-          });
-        }
-        return getCTAConfig('예약 확정', true);
-      }
-      case STATE_CODES.PAYMENT_REQUESTED:
-        return getCTAConfig('결제 요청 중', true);
-      case STATE_CODES.SHOOT_COMPLETED:
-        return getCTAConfig('리뷰 요청 완료', true);
-      case STATE_CODES.RESERVATION_CANCELED:
-        return getCTAConfig('고객님의 예약 취소', true);
-      case STATE_CODES.RESERVATION_REFUSED:
-        return getCTAConfig('예약 거절 완료', true);
-      case STATE_CODES.RESERVATION_REQUESTED:
-        return getCTAConfig('예약 요청 중', true);
-      default:
-        return getCTAConfig('', true);
-    }
-  };
-
-  const { label, disabled, onClick } = getButtonConfig();
+  const { label, disabled, onClick } = match(status)
+    .with(STATE_CODES.PHOTOGRAPHER_CHECKING, () => ({
+      label: '결제 요청하기',
+      disabled: false,
+      onClick: () => {
+        router.push(PHOTOGRAPHERS_ROUTES.PAYMENT(reservationId));
+      },
+    }))
+    .with(STATE_CODES.PAYMENT_COMPLETED, () => ({
+      label: '예약 확정하기',
+      disabled: false,
+      onClick: () => {
+        confirmReservation(reservationId);
+      },
+    }))
+    .with(STATE_CODES.RESERVATION_CONFIRMED, () =>
+      isAfterStart
+        ? {
+            label: '촬영 완료하고 리뷰 요청하기',
+            disabled: false,
+            onClick: () => {
+              completeReservation(reservationId);
+            },
+          }
+        : {
+            label: '예약 확정',
+            disabled: true,
+          },
+    )
+    .with(STATE_CODES.PAYMENT_REQUESTED, () => ({
+      label: '결제 요청 중',
+      disabled: true,
+    }))
+    .with(STATE_CODES.SHOOT_COMPLETED, () => ({
+      label: '리뷰 요청 완료',
+      disabled: true,
+    }))
+    .with(STATE_CODES.RESERVATION_CANCELED, () => ({
+      label: '고객님의 예약 취소',
+      disabled: true,
+    }))
+    .with(STATE_CODES.RESERVATION_REFUSED, () => ({
+      label: '예약 거절 완료',
+      disabled: true,
+    }))
+    .with(STATE_CODES.RESERVATION_REQUESTED, () => ({
+      label: '예약 요청 중',
+      disabled: true,
+    }))
+    .otherwise(() => ({
+      label: '',
+      disabled: true,
+    }));
 
   return (
     <>
