cd235b2d39677a02cf2641cd2466289d643f2be3
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-03-24 22:46:40 +0900

    feat(web): ProductInformation 구현

--- apps/web/src/ui/frame/product-information/ProductInformationFrame.tsx
+++ apps/web/src/ui/frame/product-information/ProductInformationFrame.tsx
@@ -1,17 +1,18 @@
 'use client';
 
 import { cn } from '@snappin/design-system/lib';
-import ImageWithShadow from '../image-with-shadow/ImageWithShadow';
-import { IconHeart, IconHeartFill } from '@snappin/design-system/assets';
-import { RemUnit } from '../types/remUnit';
-import { IconButton } from '@snappin/design-system';
+import { IconStar } from '@snappin/design-system/assets';
+import { LikeButton } from '@snappin/design-system';
 import { ProductCardProps } from '@snappin/shared/types';
+
+import ImageWithShadow from '../image-with-shadow/ImageWithShadow';
+import { RemUnit } from '../types/remUnit';
 
 type ProductInformationFrameProps = ProductCardProps & {
   isLiked: boolean;
-  imageHeight?: RemUnit;
-  imageWidth?: RemUnit;
-  handleClickLike?: () => void;
+  height?: RemUnit;
+  width?: RemUnit;
+  handleClickLike: () => void;
 };
 
 export default function ProductInformationFrame({
@@ -22,35 +23,36 @@ export default function ProductInformationFrame({
   photographer,
   price,
   isLiked,
-  imageWidth = '18.65rem',
-  imageHeight = '26.6rem',
+  width = '18.65rem',
+  height = '26.6rem',
   handleClickLike,
 }: ProductInformationFrameProps) {
-  const HeartIcon = isLiked ? IconHeartFill : IconHeart;
-
   return (
     <div className='relative w-fit overflow-hidden'>
-      <ImageWithShadow src={imageUrl} alt={alt} imageHeight={imageHeight} imageWidth={imageWidth} />
+      <ImageWithShadow src={imageUrl} alt={alt} imageHeight={height} imageWidth={width} />
       <div className='absolute right-0 bottom-0 flex w-full flex-col items-center gap-[0.8rem] p-[1.2rem]'>
-        <div className='text-black-1 flex w-full flex-col gap-[0.2rem]'>
+        <div className='text-black-1 flex w-full flex-col'>
           <p className='caption-11-md'>{name}</p>
           <span className='caption-14-bd'>{price}원~</span>
         </div>
-        <div className='text-black-4 flex w-full justify-between gap-[0.8rem]'>
-          <div className='flex min-w-0 w-full items-center gap-[0.8rem]'>
-            <span className='caption-12-md text-black-1 block min-w-0 flex-1 truncate'>
-              {photographer}
-            </span>
+
+        <div className='text-black-4 flex w-full min-w-0 items-center gap-[0.8rem]'>
+          <div className='flex min-w-0 flex-1 items-center gap-[0.8rem]'>
+            <span className='caption-9-rg block min-w-0 truncate'>{photographer}</span>
             <div className='flex shrink-0 items-center gap-[0.4rem]'>
-              <span className='caption-12-md'>{rate}</span>
-              <span className='caption-12-md'>({reviewCount})</span>
+              <div className='flex shrink-0 items-center gap-[0.2rem]'>
+                <IconStar className='h-[0.8rem] w-[0.8rem]' />
+                <span className='caption-9-rg'>{rate}</span>
+              </div>
+              <span className='caption-9-rg'>({reviewCount})</span>
             </div>
           </div>
-          <IconButton onClick={handleClickLike} aria-label={isLiked ? '좋아요 취소' : '좋아요'}>
-            <HeartIcon
-              className={cn('h-[2.4rem] w-[2.4rem]', isLiked ? 'text-neon-black' : 'text-black-1')}
-            />
-          </IconButton>
+          <LikeButton
+            isLiked={isLiked}
+            handleClick={handleClickLike}
+            aria-label={isLiked ? '좋아요 취소' : '좋아요'}
+            className={cn('h-[2.4rem] w-[2.4rem]', isLiked ? 'text-neon-black' : 'text-black-1')}
+          />
         </div>
       </div>
     </div>
