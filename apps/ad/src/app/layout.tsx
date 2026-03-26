7321cff017ea79be039001aa72defa3041570b4c
sminha <sminha8590@naver.com>
2026-03-26 22:39:13 +0900

    refactor(*): ad/src/styles 공통 패키지 리소스로 교체

--- apps/ad/src/app/layout.tsx
+++ apps/ad/src/app/layout.tsx
@@ -1,15 +1,17 @@
-import localFont from 'next/font/local';
+import { preconnect, preload } from 'react-dom';
 import { Providers } from '@/src/app/providers';
 import '@/src/styles/global.css';
 import GoogleAnalytics from '../lib/GoogleAnalytics';
 
-const suit = localFont({
-  src: '../../public/fonts/SUIT-Variable.woff2',
-});
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  preconnect('https://cdn.jsdelivr.net', { crossOrigin: 'anonymous' });
+  preload(
+    'https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css',
+    { as: 'style' },
+  );
+  
   return (
-    <html lang='ko' className={suit.className}>
+    <html lang='ko'>
       <body>
       {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
 					<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
