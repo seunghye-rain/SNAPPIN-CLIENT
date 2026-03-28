f55b26dd381a153e385f8a7305190121d0dccdf6
sminha <sminha8590@naver.com>
2026-03-28 16:53:18 +0900

    refactor(*): design-system에 ImageSlide 추가 - design-system에 ImageSlide 구현 - tagChip 스타일 추가 - web, ad에서 공통으로 사용되던 ImageSlide.mock.ts -> shared/mocks로 이동

--- packages/design-system/src/ui/image/index.ts
+++ packages/design-system/src/ui/image/index.ts
@@ -1,2 +1,3 @@
 export { default as ImagePreview } from './image-preview/ImagePreview';
 export { default as ImageWithShadow } from './image-with-shadow/ImageWithShadow';
+export { default as ImageSlide } from './image-slide/ImageSlide';
