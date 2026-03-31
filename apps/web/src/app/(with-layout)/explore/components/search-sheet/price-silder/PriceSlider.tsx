8b13cd2e9a1100729c9ce716d42566ef1ec9f99b
Tnalxmsk <alstnwkd990@naver.com>
2026-03-31 16:31:06 +0900

    feat(web): 가격 설정 슬라이더 컴포넌트 구현

---
+++ apps/web/src/app/(with-layout)/explore/components/search-sheet/price-silder/PriceSlider.tsx
@@ -0,0 +1,57 @@
+import { Slider, FilterChip } from '@snappin/design-system';
+import { ComponentProps } from 'react';
+import {
+  MAX_PRICE,
+  MIN_PRICE,
+  QUICK_MAX_PRICE,
+  QUICK_MIN_PRICE,
+  STEP,
+} from '@/app/(with-layout)/explore/constants/price';
+
+const to만원 = (value: number) => value / 10_000;
+
+type SliderProps = ComponentProps<typeof Slider>;
+type PriceSliderProps = Omit<SliderProps, 'min' | 'max' | 'step'> & {
+  min?: SliderProps['min'];
+  max?: SliderProps['max'];
+  step?: SliderProps['step'];
+};
+
+export default function PriceSlider({
+  min = MIN_PRICE,
+  max = MAX_PRICE,
+  step = STEP,
+  value,
+  onChange,
+}: PriceSliderProps) {
+  return (
+    <div className='py-[1.5rem]'>
+      <div className='flex flex-col gap-[2.4rem] px-[1.5rem] py-[1.5rem]'>
+        <span className='font-16-md text-black-10'>촬영 가격</span>
+        <div className='font-16-md text-black-10 flex justify-center gap-[0.4rem]'>
+          <div>
+            {to만원(value[0])}만원 ~ {to만원(value[1])}만원
+          </div>
+        </div>
+        <Slider min={min} max={max} step={step} value={value} onChange={onChange} />
+        <div className='flex flex-row justify-between gap-[0.6rem]'>
+          <FilterChip
+            label={`${to만원(QUICK_MIN_PRICE)}만 원 미만`}
+            isSelected={false}
+            onClick={() => onChange([0, QUICK_MIN_PRICE])}
+          />
+          <FilterChip
+            label={`${to만원(QUICK_MIN_PRICE)} ~ ${to만원(QUICK_MAX_PRICE)}만 원`}
+            isSelected={false}
+            onClick={() => onChange([QUICK_MIN_PRICE, QUICK_MAX_PRICE])}
+          />
+          <FilterChip
+            label={`${to만원(QUICK_MAX_PRICE)}만 원 이상`}
+            isSelected={false}
+            onClick={() => onChange([QUICK_MAX_PRICE, MAX_PRICE])}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/web/src/app/(with-layout)/explore/constants/price.ts
@@ -0,0 +1,9 @@
+export const MIN_PRICE = 0;
+export const MAX_PRICE = 400_000;
+export const STEP = 10_000;
+
+export const INITIAL_MIN_PRICE = 0;
+export const INITIAL_MAX_PRICE = 20_000;
+
+export const QUICK_MIN_PRICE = 100_000;
+export const QUICK_MAX_PRICE = 200_000;
