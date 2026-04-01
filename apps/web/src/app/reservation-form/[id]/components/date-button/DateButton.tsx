c523faae8a5211b34a19203fb1718f3731069ce6
bhyerain <seunghye0307@naver.com>
2026-04-02 08:39:02 +0900

    feat(web): 촬영 일정 선택 버튼

---
+++ apps/web/src/app/reservation-form/[id]/components/date-button/DateButton.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@snappin/design-system/lib/cn';
+
+type DateButtonProps = {
+  value: string;
+  hasValue: boolean;
+  handleClick: () => void;
+};
+
+export default function DateButton({ value, hasValue, handleClick }: DateButtonProps) {
+  return (
+    <button
+      type='button'
+      className={cn(
+        'bg-black-1 border-black-5 caption-14-rg w-full rounded-[0.6rem] border px-[1.2rem] py-[1rem] text-left',
+        hasValue ? 'text-black-10' : 'text-black-6',
+      )}
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
--- apps/web/src/app/reservation-form/[id]/components/index.ts
+++ apps/web/src/app/reservation-form/[id]/components/index.ts
@@ -1,2 +1,3 @@
-export {default as ClientHeader} from './client-header/ClientHeader';
-export {default as ClientFooter} from './client-footer/ClientFooter';
+export { default as ClientHeader } from './client-header/ClientHeader';
+export { default as ClientFooter } from './client-footer/ClientFooter';
+export { default as DateButton } from './date-button/DateButton';
