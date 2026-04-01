8efd5b2c014d69f541d2e8e70106e7f28b92353a
sminha <sminha8590@naver.com>
2026-04-01 20:25:27 +0900

    feat(web): ProductSection 디자인 변경사항 반영

--- apps/web/src/app/portfolio/[id]/_section/ProductSection.tsx
+++ apps/web/src/app/portfolio/[id]/_section/ProductSection.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
-import { ProductCard } from '@/ui/product-card';
+import Image from 'next/image';
+import { TagChip } from '@snappin/design-system';
+import { formatPrice } from '@snappin/shared/lib';
+import { IconStar } from '@snappin/design-system/assets';
 import { ROUTES } from '@/constants/routes/routes';
 
 type ProductSectionProps = {
@@ -24,22 +27,48 @@ export default function ProductSection({
   moods,
 }: ProductSectionProps) {
   return (
-    <section className='bg-black-1 flex flex-col gap-[1.6rem] px-[2rem] py-[1.6rem]'>
-      <h2 className='font-16-bd text-black-10'>이 작가님의 상품 살펴보기</h2>
+    <section className='bg-black-1 flex flex-col gap-[1.6rem] px-[1.8rem] py-[1.4rem]'>
+      <h2 className='font-14-bd text-black-10'>이 작가님의 상품 살펴보기</h2>
       <Link
         href={ROUTES.PRODUCT(id)}
         aria-label={`${name} 상품 상세 페이지로 이동`}
-        className='bg-black-1 border-black-4 flex flex-col items-start justify-center gap-[1rem] self-stretch rounded-[0.6rem] border-1 p-[1.2rem]'
+        className='bg-black-1 flex flex-col items-start justify-center gap-[1rem] self-stretch'
       >
-        <ProductCard
-          image={{ src: imageUrl, alt: name }}
-          name={name}
-          rate={rate}
-          reviewCount={reviewCount}
-          photographer={photographer}
-          price={price}
-          moods={moods}
-        />
+        <div className='flex gap-[1rem] w-full'>
+          {/* 좌측 상품 이미지 */}
+          <div className='relative w-[9rem] h-[9rem] shrink-0 rounded-[0.4rem] overflow-hidden'>
+            <Image
+              fill
+              alt={`${name}`}
+              src={imageUrl}
+              className='object-cover'
+            />
+          </div>
+          {/* 우측 상품 정보 */}
+          <div className='flex flex-col gap-[0.8rem] min-w-0'>
+            {/* 이름, 가격 */}
+            <div className='flex flex-col gap-[0.2rem] text-black-10'>
+              <span className='caption-12-md truncate'>{name}</span>
+              <span className='font-16-sb truncate'>{formatPrice(price)}원~</span>
+            </div>
+            {/* 무드, 작가, 별점, 리뷰 */}
+            <div className='flex flex-col gap-[0.8rem]'>
+              <div className='flex gap-[0.4rem]'>
+                {moods.map((mood) => <TagChip key={mood} variant='gray' label={mood} />)}
+              </div>
+              <div className='flex gap-[1.2rem] caption-12-rg text-black-7'>
+                <span>{photographer}</span>
+                <div className='flex gap-[0.6rem]'>
+                  <div className='flex gap-[0.2rem]'>
+                    <IconStar className='w-[1rem] h-full'/>
+                    <span>{rate}</span>
+                  </div>
+                  <span>리뷰 {reviewCount}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Link>
     </section>
   );
