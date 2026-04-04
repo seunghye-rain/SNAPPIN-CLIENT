b1f58b3542d2c016fe878869ce772fa2ef3fd9ca
bhyerain <seunghye0307@naver.com>
2026-04-04 14:17:39 +0900

    refactor(web): 모달 위치 변경

--- apps/web/src/app/product/[id]/reservation-form/components/client-footer/ClientFooter.tsx
+++ apps/web/src/app/product/[id]/reservation-form/components/client-footer/ClientFooter.tsx
@@ -1,56 +1,37 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import { BottomCTAButton } from '@snappin/design-system';
-import { ROUTES } from '@/constants/routes/routes';
-import CopyModal from '../../@modal/(.)copy-modal/CopyModal';
 
 type ClientFooterProps = {
-  photographerId: number;
   disabled?: boolean;
   handleClick?: () => Promise<boolean>;
+  handleCopySuccess?: () => void;
 };
 
 export default function ClientFooter({
-  photographerId,
   disabled = false,
   handleClick,
+  handleCopySuccess,
 }: ClientFooterProps) {
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
-
   const handleCopyButtonClick = async () => {
     const isCopied = handleClick ? await handleClick() : false;
     if (isCopied) {
-      setOpen(true);
+      handleCopySuccess?.();
     }
   };
 
-  const handleMovePhotographer = () => {
-    setOpen(false);
-    router.push(ROUTES.PHOTOGRAPHER(photographerId));
-  };
-
   return (
-    <>
-      <div className='h-[8.4rem]'>
-        <BottomCTAButton background='white' hasPadding fixed>
-          <BottomCTAButton.Single
-            color='black'
-            size='large'
-            disabled={disabled}
-            onClick={handleCopyButtonClick}
-          >
-            신청 양식 복사하기
-          </BottomCTAButton.Single>
-        </BottomCTAButton>
-      </div>
-      <CopyModal
-        open={open}
-        handleOpenChange={setOpen}
-        handleClickConfirm={handleMovePhotographer}
-      />
-    </>
+    <div className='h-[8.4rem]'>
+      <BottomCTAButton background='white' hasPadding fixed>
+        <BottomCTAButton.Single
+          color='black'
+          size='large'
+          disabled={disabled}
+          onClick={handleCopyButtonClick}
+        >
+          신청 양식 복사하기
+        </BottomCTAButton.Single>
+      </BottomCTAButton>
+    </div>
   );
 }
--- apps/web/src/app/product/[id]/reservation-form/components/client-header/ClientHeader.tsx
+++ apps/web/src/app/product/[id]/reservation-form/components/client-header/ClientHeader.tsx
@@ -1,41 +1,27 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import { IconArrowBack } from '@snappin/design-system/assets';
 import { IconButton, Navigation } from '@snappin/design-system';
-import CancelModal from '../../@modal/(.)cancel-modal/CancelModal';
 
-export default function ClientHeader() {
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
+type ClientHeaderProps = {
+  handleClickBack: () => void;
+};
 
-  const handleBackClick = () => {
-    setOpen(true);
-  };
-
-  const handleExit = () => {
-    router.back();
-    setOpen(false);
-  };
-
+export default function ClientHeader({ handleClickBack }: ClientHeaderProps) {
   return (
-    <>
-      <Navigation
-        left={
-          <IconButton onClick={handleBackClick} className='h-[2.4rem] w-[2.4rem]'>
-            <IconArrowBack />
-          </IconButton>
-        }
-        center={
-          <span className='font-16-bd text-black-10 block max-w-[20rem] truncate'>
-            예약 신청 양식 작성하기
-          </span>
-        }
-        className='border-black-5 border-b'
-        isFixed
-      />
-      <CancelModal open={open} handleOpenChange={setOpen} handleClickExit={handleExit} />
-    </>
+    <Navigation
+      left={
+        <IconButton onClick={handleClickBack} className='h-[2.4rem] w-[2.4rem]'>
+          <IconArrowBack />
+        </IconButton>
+      }
+      center={
+        <span className='font-16-bd text-black-10 block max-w-[20rem] truncate'>
+          예약 신청 양식 작성하기
+        </span>
+      }
+      className='border-black-5 border-b'
+      isFixed
+    />
   );
 }
