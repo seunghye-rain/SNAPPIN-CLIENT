8df384fd1676e46a6e3f8b8297353c98fb7080fb
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-04-06 20:54:37 +0900

    refactor(web): 쿼리키 수정

--- apps/web/src/queries/mood.ts
+++ apps/web/src/queries/mood.ts
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { SERVER_API_BASE_URL } from '@/api/constants/api';
 import { GetMoodFilterResponse } from '@/swagger-api';
+import { USER_QUERY_KEY } from '@/query-key/user';
 
 export const useGetMoodIdList = () => {
   //토큰 불필요 api 이므로 fetch 사용
   return useQuery<GetMoodFilterResponse[]>({
-    queryKey: ['moods'],
+    queryKey: USER_QUERY_KEY.MOODS,
     queryFn: () =>
       fetch(`${SERVER_API_BASE_URL}/api/v2/moods`, {
         method: 'GET',
