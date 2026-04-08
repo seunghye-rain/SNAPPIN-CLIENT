9af4848d37808630eee666274aa132da887a96ae
Tnalxmsk <alstnwkd990@naver.com>
2026-04-08 21:52:58 +0900

    feat(web): 좋아요 스켈레톤 교체

--- apps/web/src/app/(with-layout)/like/components/LikeTabContents.tsx
+++ apps/web/src/app/(with-layout)/like/components/LikeTabContents.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { PortfolioListSkeleton, ProductListSkeleton } from '@/ui';
+import { PortfolioFrameListSkeleton, ProductFrameListSkeleton } from '@/ui';
 import PortfolioListSection from '../_section/PortfolioListSection';
 import ProductListSection from '../_section/ProductListSection';
 import { LIKE_TAB, type LikeTab } from '../constants/tab';
@@ -13,7 +13,7 @@ export default function LikeTabContents({ currentTab }: LikeTabContentsProps) {
     <div className='scrollbar-hide bg-black-1 min-h-0 overflow-y-hidden'>
       {currentTab === LIKE_TAB.PORTFOLIO && (
         <section className='min-h-full'>
-          <Suspense fallback={<PortfolioListSkeleton className='p-[1rem]' />}>
+          <Suspense fallback={<PortfolioFrameListSkeleton />}>
             <PortfolioListSection />
           </Suspense>
         </section>
@@ -22,8 +22,8 @@ export default function LikeTabContents({ currentTab }: LikeTabContentsProps) {
         <section className='bg-black-1 min-h-full'>
           <Suspense
             fallback={
-              <div className='bg-black-1 flex h-full flex-col gap-[0.6rem]'>
-                <ProductListSkeleton length={5} />
+              <div className='bg-black-1 h-full'>
+                <ProductFrameListSkeleton length={6} />
               </div>
             }
           >
